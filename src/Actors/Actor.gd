41b5dde93000e4e9e6f210ccb7625ad467f091e8
Moeva <mondohamoeva@hotmail.fr>
2023-03-02 08:41:54 +0100

    {add input}

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -2,12 +2,13 @@ extends CharacterBody2D
 class_name Actor
 
 @export_category("character Movement")
-@export var speed = Vector2(300.0,10000)
-@export var gravity: = 3000.0
+@export var speed = Vector2(300.0,1000)
+@export var gravity: = 850
 
 var vel: = Vector2.ZERO
 
-func  _physics_process(delta: float) -> void :
+func  _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
+#	velocity.y = max(velocity.y, speed.y)
 	set_velocity(velocity)
 	move_and_slide()
